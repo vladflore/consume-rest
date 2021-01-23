1a19b59e84974c4e7b97aedd4934ea9f2a21eed6
Vlad Flore <flore.vlad@gmail.com>
2021-01-24 00:17:08 +0100

    Initialize spring boot app

---
+++ consume-rest-spring/src/main/java/tech/vladflore/consumerest/ConsumeRestApplication.java
@@ -0,0 +1,13 @@
+package tech.vladflore.consumerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeRestApplication.class, args);
+	}
+
+}
---
+++ consume-rest-spring/src/test/java/tech/vladflore/consumerest/ConsumeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.vladflore.consumerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
