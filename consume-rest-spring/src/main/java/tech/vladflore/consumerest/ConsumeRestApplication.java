8bb8a6cff3a58a30859e17a44169412317549642
Vlad Flore <flore.vlad@gmail.com>
2021-01-24 12:32:20 +0100

    Organize imports

--- consume-rest-spring/src/main/java/tech/vladflore/consumerest/ConsumeRestApplication.java
+++ consume-rest-spring/src/main/java/tech/vladflore/consumerest/ConsumeRestApplication.java
@@ -1,7 +1,5 @@
 package tech.vladflore.consumerest;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
